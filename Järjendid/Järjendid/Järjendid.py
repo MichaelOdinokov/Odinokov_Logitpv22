386ad6d5c95c637c59599ec0b7d559d4d76f0017
opilane <opilane@HP-CZC2349HTM>
2023-01-25 15:44:30 +0200

    12

--- Järjendid/Järjendid/Järjendid.py
+++ Järjendid/Järjendid/Järjendid.py
@@ -6,14 +6,21 @@ slovo_list=list(slovo)
 print(slovo)
 print(slovo_list)
 while True:
-    print("1-lisa sõna lists")
-    print("2-kleebi järendid")
-    print("4-udalit element")
+    print("1- lisa sõna lists")
+    print("2 - kleebi järendid")
+    print("4 - udalit element")
+    print("5 - Удаляет i-ый элемент и возвращает его.")
+    print("6 - Возвращает положение первого элемента от start до end со значением a ") 
+    print("7 - Копия списка ")
     valik=int(input())
     if valik==1:
         a=input("Sisse bukvu")
         slovo_list.append(a)
         print(f"dobavil {a} uus järendid", slovo_list)
+    elif valik==5:
+        slovo_list.pop(a)
+    elif valik==6:
+        slovo_list.index(a, [start [, end]])
     elif valik==2:
         slovo_list.extend(abc)
         print(slovo_list)
@@ -31,5 +38,7 @@ while True:
         else:
             print("Iskomoi bukvi net")
     print(slovo_list)
+
+    
 
 
