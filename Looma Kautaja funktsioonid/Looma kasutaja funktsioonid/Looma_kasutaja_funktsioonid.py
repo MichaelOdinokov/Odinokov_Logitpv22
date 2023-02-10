43546069efd4defc4efe6232bae3920b9eb08b15
opilane <opilane@HP-CZC2349HTM>
2023-02-10 13:13:55 +0200

    2

---
+++ Looma Kautaja funktsioonid/Looma kasutaja funktsioonid/Looma_kasutaja_funktsioonid.py
@@ -0,0 +1,33 @@
+from Omamodul import *
+
+
+#square
+i=5
+P=i**4
+S=i**2
+d=P/4
+print(f"S
+
+
+#arithmetic
+arithmetic=a
+print(a)
+aa=y
+
+
+
+# is_year_leap
+while True:
+    aa=int(input("Mis aastat täna on?"))
+    if aa==2023:
+        print(aa)#return
+    else:
+        print("Viga")
+
+
+    
+
+
+
+
+
---
+++ Looma Kautaja funktsioonid/Looma kasutaja funktsioonid/Omamodul.py
@@ -0,0 +1,31 @@
+
+
+
+
+def arithmetic(arv1:float,tehe: str,arv2:float)->any:
+    """E
+    :param int a: Järjend 
+    :rtype: str
+    """
+
+    if tehe="+":
+        vastus=a+a2
+    print(c)
+    d=a*b
+    print(d)
+    e=a/b
+    print(e)
+    return 
+
+def  y(is_year_leap:int)->str:
+    """E
+    :param int y: Järjend
+    :rtype: str
+    """
+    if aa=2023
+
+def k(square)->str:
+    """
+    """
+ 
+    
