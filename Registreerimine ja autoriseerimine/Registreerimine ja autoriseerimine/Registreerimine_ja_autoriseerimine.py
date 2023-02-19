d1a60de10f2c1391948432656ae27bbbaf5021b7
Michael Odinokov <mihhail.odinokov@gmail.com>
2023-02-19 17:46:41 +0200

    12

--- Registreerimine ja autoriseerimine/Registreerimine ja autoriseerimine/Registreerimine_ja_autoriseerimine.py
+++ Registreerimine ja autoriseerimine/Registreerimine ja autoriseerimine/Registreerimine_ja_autoriseerimine.py
@@ -4,7 +4,7 @@ p=[]
 
 while True:
     l=log(int(input("1- Sisse, 2-registreerimine:")))
-    if l==1:
+    if l==2:
         nimi=alogin(int("Sisse sinu nimi:"),salasona=int(input("Sisesta ma salsına:")))
         pm=mp(int(input("1-parool ja nimi muutuja, 2 - ei taha")))
         if pm==1:
@@ -12,6 +12,14 @@ while True:
             print(nimi)
         elif pm==2:
             continue
+    elif l==1:
+        n=int(input("1-V‰lja, 2-j‰‰: "))
+        if n==1:
+            break
+        elif n==2:
+            continue
+
+
 
         
 
