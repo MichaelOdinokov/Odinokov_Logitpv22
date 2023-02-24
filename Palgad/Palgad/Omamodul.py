e0b4851489b5adf4ae579057e986508a21f8c408
Michael Odinokov <mihhail.odinokov@gmail.com>
2023-02-24 22:42:59 +0200

    12

--- Palgad/Palgad/Omamodul.py
+++ Palgad/Palgad/Omamodul.py
@@ -1,44 +1,26 @@
-﻿palgad=[1200,2500,750,395,1200]
-inimesed=["A","B","C","D","E","F","G"]
+﻿import random
+from tkinter import W 
+palgad=[1200,2500,750,395,1200]
+inimesed=["A","B","C","D","E"]
 
-def k(k:bool,pp:int)->bool:
+def k(k:list):
     """
     Kasutajad kirjuta ise nimi
     :parem k: Muutaja
     :rtype: bool
     """
-    k=bool(input("Sisse sinu nimi:"))
-    k.append
-    if k in inimesed and inimesed.istitle():# Начинаются ли слова в строке с заглавной буквы: (inimesed.istitle())
+    
+    if k not in inimesed:# Начинаются ли слова в строке с заглавной буквы: (inimesed.istitle())
+        inimesed.append(k)
         pp=int(input("Sisse teie palgad: "))
-        print(pp)
+        print("Teie palg on",pp)
+        palgad.append(pp)
         pass
     else:
         print("Viga!")
-    return k, pp
+    return k
 
 #19 Ise funktsioon, uus töötane
-
-def uk(u:int, t: list):
-    """
-    Uus töötane registrerimine
-    :parem bool u: Muutaja
-    :rtype: u
-    """
-    va=int(input("Teie vanus: "))
-    if va<18:
-        print("Te ei soobi!")
-    elif va>=18:
-        u=bool(input("Sisse teie nimi: "))
-        t.append(u)
-        p=int(input("Milline palg te tahte, suurem 1200 euro või väiksem(1-Suurem, 2-Väikem):"))
-        if p==1:
-            print("Me helistame tagasi teiele!")    
-        elif p==2:
-            print("Teie palg on 1300 eurp.Palju õnne!")
-            pa.append(v)
-    return u, p
-
 def valik(v:int):
     """
     Vale töölt lahkuda
@@ -46,15 +28,37 @@ def valik(v:int):
     :rtype:
     """
     if v==2:
-        uk
+        va=int(input("Teie vanus: "))
+        if va<18:
+            print("Te ei soobi!")
+        elif va>=18:
+            u=input("Sisse teie nimi: ")
+            inimesed.append(u)
+            pp=int(input("Sisse teie palgad: "))
+            palgad.append(pp)
+            print("Teie palg on",pp)
     elif v==1:
-        nimi=bool(input("Sisse sinu nimi:"))
-        if nimi in t:
-            t.removed(nimi)
-            print("Sinu nimi removed. Nägemist!")
+        nimi=input("Sisse sinu nimi:")
+        if nimi in inimesed:
+            inimesed.removed(nimi)
+            print("Sinu nimi removed.")
+            pp=int(input("Sisse teie palgad: "))
+            palgad.append(pp)
+            print("Teie palg removed.")
+            print("Nägemist!")
         else:
             print("Te ei töötate siin!")
     return v
+
+#Premium võta
+def premium(w:int):
+    if w==1:
+        for i in range(1):
+            w=random.choice(inimesed)
+            print("Победитель:", w)
+    else:
+        print("Kirjuta 1!")
+
 
 #def delete
 #def create
--- Palgad/Palgad/Palgad.py
+++ Palgad/Palgad/Palgad.py
@@ -4,15 +4,11 @@ palgad=[1200,2500,750,395,1200,850,3000]
 inimesed=["A","B","C","D","E","F","G"]
 
 while True:
-    try:
-        kk=k(bool(input("Sisse sinu nimi:")))
-    except:
-        print("Viga")
-    print()
-    #19 Ise funktsioon, uus töötane
-    print("Tere tulemst!")
-    v=valik(int(input("1-töölt lahkuda, 2-tööd leidma")))
-    va=uk(int(input("Teie vanus: ")))
+        kk=k(input("Sisse sinu nimi:"))
+        print()
+        v=valik(int(input("1-töölt lahkuda, 2-tööd leidma")))
+        e=premium(int(input("Kellel on lisatasu 1. klahvi aktiveerimiseks:")))
+
     
 
 
