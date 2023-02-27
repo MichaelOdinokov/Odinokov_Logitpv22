3c626bead6685e5ef6fa83952237542a45fdf26b
opilane <opilane@HP-CZC2349HTM>
2023-02-27 12:32:55 +0200

    12

--- Palgad/Palgad/Omamodul.py
+++ Palgad/Palgad/Omamodul.py
@@ -1,31 +1,86 @@
 ﻿import random
-from tkinter import W 
-palgad=[1200,2500,750,395,1200]
-inimesed=["A","B","C","D","E"]
 
-def k(k:list):
+def Vordsed_palgad(inimesed: list, palgad: list):#
+    """
+    """
+    dublikatid=[x for x in palgad if palgad.count(x)>1]
+    dublikatid=list(set(dublikatid))# lisa Järendisse: [1200, 2500, 750, 750, 1200]->[1200,750]
+    for palk in dublikatid:
+        n=palgad.count(palk)#1200, n=2; 750, n=2
+        k=-1#
+        print(f"{palk} saavad kätte järgmised inimesed:")
+        for j in range(n):
+            k=palgad.index(palk, k+1)# k=p.index(palk, k+1)- pervi index ctobi pereschla na sledujushi index to stavim satrtovi index
+            nimi=inimesed[k]
+            print(nimi)
+    return palgad, inimesed
+
+def Sorteerimine(inimesed: list, palgad: list):
+    """Palgad kahneb või kadvab
+    param: list inimesed, list palgad: Järjend
+    :rtype: list, list
+    """
+    v=int(input("palk 1-kahaneb, 2-kadvab?"))
+    if v==1:
+        n=len(palgad)
+        for j in range (0, n-1):
+            for k in range (j+1,n):
+                if palgad[j]<palgad[k]:
+                    abi=palgad[j]# if abi 0 palgad[j]
+                    palgad[j]=palgad[k]# if palgad[j]=palgad[k]
+                    palgad[k]=abi# if palgad[k]=abi
+                    abi=inimesed[j]# if  abi=inimesed[j]
+                    inimesed[j]=inimesed[k]# if inimesed[j]=inimesed[k]
+                    inimesed[k]=abi# if inimesed[k]=abi
+    elif v==2:
+            n=len(palgad)
+            for j in range (0, n-1):
+                for k in range (j+1,n):
+                    if palgad[j]>palgad[k]:
+                        abi=palgad[j]
+                        palgad[j]=palgad[k]
+                        palgad[k]=abi
+                        abi=inimesed[j]
+                        inimesed[j]=inimesed[k]
+                        inimesed[k]=abi
+    return inimesed, palgad
+
+                
+def Kustutamine(inimesed: list, palgad: list):
+    """
+    :param list i: Inimeste järjend, lisy p: Palgade järjend
+    :rtype: list, list
+    """
+    nimi=input("Sisesta nimi:")
+    if nimi in inimesed:
+        n=inimesed.count(nimi)# list.count shitaet cloichestvo zarplat
+        for j in range(n):
+            ind=inimesed.index(nimi)
+            inimesed.pop(ind)
+            palgad.pop(ind)
+    return inimesed, palgad
+
+def k(k:str, palgad: list, inimesed: list):
     """
     Kasutajad kirjuta ise nimi
-    :parem k: Muutaja
-    :rtype: bool
-    """
-    
+    :parem k: str, palgad: list, inimesed: list
+    :rtype: palgad, inimesed
+    """    
     if k not in inimesed:# Начинаются ли слова в строке с заглавной буквы: (inimesed.istitle())
-        inimesed.append(k)
-        pp=int(input("Sisse teie palgad: "))
-        print("Teie palg on",pp)
-        palgad.append(pp)
-        pass
+            inimesed.append(k)
+            pp=int(input("Sisse teie palgad: "))
+            print("Teie palg on",pp)
+            palgad.append(pp)
     else:
-        print("Viga!")
-    return k
+        print("Lisa sinu nimi!")
+    return inimesed, palgad
 
 #19 Ise funktsioon, uus töötane
-def valik(v:int):
+def valik(v:int, inimesed: list, palgad: list):
     """
     Vale töölt lahkuda
     :parm int: v: Muutaja
-    :rtype:
+    :rtype: inimesed, palgad
     """
     if v==2:
         va=int(input("Teie vanus: "))
@@ -40,39 +95,23 @@ def valik(v:int):
     elif v==1:
         nimi=input("Sisse sinu nimi:")
         if nimi in inimesed:
-            inimesed.removed(nimi)
+            inimesed.remove(nimi)
             print("Sinu nimi removed.")
             pp=int(input("Sisse teie palgad: "))
-            palgad.append(pp)
+            palgad.remove(pp)
             print("Teie palg removed.")
             print("Nägemist!")
         else:
             print("Te ei töötate siin!")
-    return v
+    return inimesed, palgad
 
 #Premium võta
-def premium(w:int):
+"""
+"""
+def premium(w:int, inimesed: list):
     if w==1:
         for i in range(1):
             w=random.choice(inimesed)
             print("Победитель:", w)
     else:
         print("Kirjuta 1!")
-
-
-#def delete
-#def create
-
-
-
-            
-
-    
-
-    
-
-
-
-
-
-        
--- Palgad/Palgad/Palgad.py
+++ Palgad/Palgad/Palgad.py
@@ -1,15 +1,19 @@
 from Omamodul import *
+palgad=[1200,2500,750,395,1200]
+inimesed=["A","B","C","D","E"]
 
-palgad=[1200,2500,750,395,1200,850,3000]
-inimesed=["A","B","C","D","E","F","G"]
+v=Sorteerimine(int(input("palk 1-kahaneb, 2-kadvab?")), inimesed, palgad)
 
 while True:
-        kk=k(input("Sisse sinu nimi:"))
+    try:
+        kk=k(input("Sisse sinu nimi:"),palgad,inimesed)
+        
+    except:
+        print("Lisa sinu nimi!")
+
         print()
-        v=valik(int(input("1-töölt lahkuda, 2-tööd leidma")))
-        e=premium(int(input("Kellel on lisatasu 1. klahvi aktiveerimiseks:")))
-
-    
-
-
-
+        inimesed, palgad=valik(int(input("1-töölt lahkuda, 2-tööd leidma")), inimesed, palgad)    
+        print(inimesed)
+        print(palgad)
+        print()
+        e=premium(int(input("Kellel on lisatasu 1. klahvi aktiveerimiseks:")), inimesed)
